f89432dde54983f96de292ac46a93ed07ade8648
Yassine Elouafi <yelouafi@gmail.com>
2018-09-11 13:17:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,113 @@
+
+function isGenerator(x) {
+    return x != null && typeof x.next === 'function'
+}
+
+function isHandler(x) {
+    return x != null && x.__ISHANDLER === true
+}
+
+function isOp(x) {
+    return x != null && x.__ISOP === true
+}
+
+function isCps(x) {
+    return x != null && x.__ISCPS === true
+}
+
+function op(type, data) {
+    return { __ISOP: true, type, data }
+}
+
+function withHandler(handler, computation) {
+    const handlerComp = { __ISHANDLER: true, handler, computation }    
+    computation._return = handlerComp
+    return handlerComp
+}
+
+function cps(func) {
+    return { __ISCPS: true, func }
+}
+
+function start(computation) {
+    if(isGenerator(computation)) {
+        resumeGenerator(computation, null)
+    }
+    else if(isHandler(computation)) {        
+        start(computation.computation)
+    }
+    else if(isOp(computation)) {
+        startOp(computation)
+    }
+    else if(isCps(computation)) {
+        computation.func(computation._return)
+    }
+}
+
+function resume(computation, result) {
+    if(isGenerator(computation)) {
+        resumeGenerator(computation, result)
+    }
+    else if(isHandler(computation)) {
+        resumeHandler(computation, result)
+    }
+    else if(isOp(computation)) {
+        resume(computation._return, result)
+    }
+    else if(typeof computation === 'function') {
+        computation(result)
+    }   
+}
+
+function resumeHandler({handler, _return}, result) {
+    if(handler.return != null) {
+        const returnComp = handler.return(result)
+        returnComp._return = _return
+        start(returnComp)
+    }
+    else {
+        resume(_return, result)
+    }
+}
+
+
+function resumeGenerator(gen, arg) {
+    const {done, value} = gen.next(arg)
+    if(done) {
+        resume(gen._return, value)
+    } else {        
+        value._return = gen
+        start(value)
+    }
+}
+
+function startOp(op) {
+    const startComp = op._return
+    let currentComp = startComp
+    let fh = isHandler(currentComp) ? startComp.handler[op.type] : null
+    while(fh == null) {
+        currentComp = currentComp._return
+        if(currentComp == null) break
+        fh = isHandler(currentComp) ? currentComp.handler[op.type] : null
+    }
+    if(!fh) throw new Error('Unhandled effect ' + op.type)
+           
+    const handlerReturn = currentComp._return
+    currentComp._return = null
+
+    const handlerComp = fh(
+        op.data, 
+        function resumeCont(value) {
+            return cps(parentComp => {
+                if(parentComp)
+                    currentComp._return = parentComp
+                resume(startComp, value)
+            })
+        }
+    )
+    if(currentComp._return === null) {
+        currentComp._return = handlerComp
+    }
+    handlerComp._return = handlerReturn
+    start(handlerComp)
+}
